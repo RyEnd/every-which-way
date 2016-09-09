3056c1fced36362749fab8b0ac1c79fd105af78f
Ryan Molnar <ryanbmolnar@gmail.com>
2016-09-09 16:25:29 -0400

    algos through binary search

---
+++ Algos/Union-find.java
@@ -0,0 +1,14 @@
+public class UF(int N){
+	void union(int p, int q){
+		//connect p to q
+	};
+	boolean connected(int p, int q){
+		//is p connected to q
+	};
+	int find(int p){
+		//
+	};
+	int count(){
+
+	};
+}
