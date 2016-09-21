90c32f02eea1ed6cf592c7c590b25f2ccbf82f59
Ryan Molnar <ryanbmolnar@gmail.com>
2016-09-21 10:21:32 -0600

    getting started with dw-getting-started

---
+++ webdev/DWGettingStarted/src/main/java/com/javaeeeee/dwstart/resources/HelloResource.java
@@ -0,0 +1,21 @@
+package com.javaeeeee.dwstart.resources;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author ryan
+ */
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getGreeting() {
+        return "Hello world!";
+    }
+}
