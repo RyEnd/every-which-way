d69103243a9e5799857cfa29d177957ef17a17b1
Ryan Molnar <ryanbmolnar@gmail.com>
2016-09-26 16:15:37 -0600

    complete variety of greeting APIs and tests

---
+++ webdev/DWGettingStarted/src/main/java/com/javaeeeee/dwstart/core/Greeting.java
@@ -0,0 +1,24 @@
+package com.javaeeeee.dwstart.core;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ *
+ * @author ryan
+ */
+public class Greeting {
+
+    @JsonProperty
+    private String greeting;
+
+    public Greeting() {
+    }
+
+    public Greeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
