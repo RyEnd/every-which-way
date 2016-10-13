6ac6126af12e0401fd0f6ab9a32b697b9dec66f1
Ryan Molnar <ryanbmolnar@gmail.com>
2016-10-13 13:01:55 -0400

    s'more go 'cisms

--- Exercisms/go/hello-world/hello_world.go
+++ Exercisms/go/hello-world/hello_world.go
@@ -2,11 +2,10 @@ package hello
 
 const testVersion = 2
 
-import "fmc"
 // HelloWorld takes in a string, then responds "Hello {string}!" or "Hello world!" if no string is entered or the entry was invalid.
-func HelloWorld(string) string {
-	fmt.Println("Hello " + string + "!")
-}
-func HelloWorld() string {
-	fmt.Println("Hello world!")
+func HelloWorld(name string) string {
+	if name == "" {
+		name = "World"
+	}
+	return ("Hello, " + name + "!")
 }
